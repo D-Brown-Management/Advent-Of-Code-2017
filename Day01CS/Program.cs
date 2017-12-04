1681f97d48d3db8fa7e8cb748cf5bd928490878d
Steve Jost <steve@stevejost.com>
2017-12-03 19:23:48 -0800

    Initial Check-In

---
+++ Day01CS/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day01CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PartOne();
+            PartTwo();
+        }
+
+        static void PartTwo()
+        {
+            var input = File.ReadAllText("input.txt");
+            var charArray = input.ToCharArray();
+            int sum = 0;
+
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                int charValue = int.Parse(charArray[i].ToString());
+                if (i >= charArray.Length/2)
+                {
+                    if (charArray[i] == charArray[i - (charArray.Length / 2)])
+                    {
+                        sum += charValue;
+                    }
+                }
+                else
+                {
+                    if (charArray[i] == charArray[i + (charArray.Length/2)])
+                    {
+                        sum += charValue;
+                    }
+                }
+
+            }
+
+            Console.WriteLine($"Part 2 Answer: {sum}");
+
+        }
+        static void PartOne()
+        {
+            var input = File.ReadAllText("input.txt");
+            int sum = 0;
+
+            var charArray = input.ToCharArray();
+
+            for (int i = 0; i < charArray.Length; i++)
+            {
+                int charValue = int.Parse(charArray[i].ToString());
+                if (i + 1 == charArray.Length)
+                {
+                    if (charArray[i] == charArray[0])
+                    {
+                        sum += charValue;
+                    }
+                }
+                else
+                {
+                    if (charArray[i] == charArray[i + 1])
+                    {
+                        sum += charValue;
+                    }
+                }
+                
+            }
+
+            Console.WriteLine($"Part 1 Answer: {sum}");
+        }
+    }
+}
---
+++ Day02CS/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day02CS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PartOne();
+            PartTwo();
+        }
+
+        private static void PartTwo()
+        {
+            int checksum = 0;
+            var lines = File.ReadAllLines("input.txt");
+
+            foreach (var line in lines)
+            {
+                checksum += RowChecksumTwo(line);
+            }
+
+            Console.WriteLine($"Part 2: {checksum}");
+        }
+
+        private static int RowChecksumTwo(string row)
+        {
+            string[] numbers = row.Split('\t');
+            int[] numbersInts = new int[numbers.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbersInts[i] = int.Parse(numbers[i]);
+            }
+
+            for (int j = 0; j < numbersInts.Length; j++)
+            {             
+
+                for (int k = 0; k < numbersInts.Length; k++)
+                {
+                    if (j == k)
+                    {
+                        continue;
+                    }
+
+                    if (numbersInts[j] % numbersInts[k] == 0)
+                    {
+                        return numbersInts[j] / numbersInts[k];
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        static void PartOne()
+        {
+            int checksum = 0;
+            var lines = File.ReadAllLines("input.txt");
+
+            foreach (var line in lines)
+            {
+                checksum += RowChecksum(line);
+            }
+
+            Console.WriteLine($"Part 1: {checksum}");
+        }
+
+        static int RowChecksum(string row)
+        {
+            int min = 99999999;
+            int max = 0;
+            string[] numbers = row.Split('\t');
+            int[] numbersInts = new int[numbers.Length];
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbersInts[i] = int.Parse(numbers[i]);
+            }
+
+            for (int j = 0; j < numbersInts.Length; j++)
+            {
+                if (numbersInts[j] > max)
+                {
+                    max = numbersInts[j];
+                }
+
+                if (numbersInts[j] < min)
+                {
+                    min = numbersInts[j];
+                }
+            }
+
+            
+
+
+            return max-min;
+        }
+    }
+}
